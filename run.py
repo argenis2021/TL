95f096909bc8084afc1d87291fcc403bd868e74b
argenis2021 <argenismoncada@gmail.com>
2020-01-03 12:36:42 -0400

    change Io Route google def

--- run.py
+++ run.py
@@ -79,14 +79,14 @@ def Importar():
 def Google():
 
     f1 = open('/tmp/tel_todo.csv', 'r')
-    f2 = open('google_tel_todo.csv', 'w')
+    f2 = open('/home/argenis/apps/TL_IO/google_tel_todo.csv', 'w')
     entrada = csv.DictReader(f1)
     f2.write('Name,Given Name,Additional Name,Family Name,Yomi Name,Given Name Yomi,Additional Name Yomi,Family Name Yomi,Name Prefix,Name Suffix,Initials,Nickname,Short Name,Maiden Name,Birthday,Gender,Location,Billing Information,Directory Server,Mileage,Occupation,Hobby,Sensitivity,Priority,Subject,Notes,Language,Photo,Group Membership,Phone 1 - Type,Phone 1 - Value'+'\n')
     for reg in entrada:
         f2.write('11AA '+reg['cliente']+',,,,,,,,,,,,,,,,,,,,,,,,,Avenida B,,,* Avenida B ::: * Avenida B,Mobile,'+reg['telefono']+'\n')
     f1.close()
     f2.close()
-    print ("Archivo google_tel_todo.csv disponible en Documentos")
+    print ("Archivo google_tel_todo.csv disponible en apps/TL_IO/")
     input()
     os.system('clear')
     
