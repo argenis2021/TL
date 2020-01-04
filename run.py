26246a4efcf887f61f2ac80ad4f04d0c5f8d0b7b
argenis2021 <argenismoncada@gmail.com>
2020-01-03 20:54:19 -0400

    include import as module

--- run.py
+++ run.py
@@ -60,28 +60,7 @@ def Volumen():
 
 def Importar():
     os.system("python change_coma_dot.py")
-#connect to the database
-    conn = psycopg2.connect(host='localhost',
-                            dbname='todolimpiecito',
-                            user='postgres',
-                            password='')
-    #create a cursor object 
-    #cursor object is used to interact with the database
-    cur = conn.cursor()
-    
-    #/home/argenis/apps/TL_IO/ventas.csv
-    in_file = open('/tmp/ventas.csv.tmp', 'r')
-    
-    # FIRST LINE IN FILE ARE COLUMN NAMES
-    # TURN FIRST LINE INTO AN ARRAY
-    columns = in_file.readline().strip('\n').split(',')
-    # LOAD COPY STARTING AT SECOND LINE IN FILE
-    cur.copy_from(in_file, 'ventas_la_carlota', sep=',', columns=columns)
-    # Cerramos la conexión
-    conn.commit()
-    conn.close()
-    input()
-    os.system('clear')
+    os.system("python import_data.py")
 
 def Google():
 
