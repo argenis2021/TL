e8b055e1db0dec101c18975189d2b2c0753a33e8
argenis2021 <argenismoncada@gmail.com>
2020-01-03 20:05:46 -0400

    change doble quote

--- change_coma_dot.py
+++ change_coma_dot.py
@@ -1,30 +1,47 @@
 import shutil, os
 from os import remove	
 def swapDotComa(line):
-     """ Search for a coma inside doble quoted words and change it to dot.
-     this may be used to change LATIN , used in decimal for . used in
-     American notation."""
+    """ Search for a coma inside doble quoted words and change it to dot.
+    this may be used to change LATIN , used in decimal for . used in
+    American notation."""
 
 
-     # We start outside a doble quote
-     quoted = False
-     line = list(line)
-     for i in range(len(line)-1):
-         if line[i]== '"':
-             quoted = not quoted
-         elif quoted and line[i]==',':
-             line[i] = '.'
-         elif quoted and line[i]=='.':
-             line[i] = ','
-     return ''.join(line)
+    # We start outside a doble quote
+    quoted = False
+    line = list(line)
+    for i in range(len(line)-1):
+        if line[i]== '"':
+            quoted = not quoted
+        elif quoted and line[i]==',':
+            line[i] = '.'
+        elif quoted and line[i]=='.':
+            line[i] = ','
+    return ''.join(line)
+
+def deleteDobleQuoted(line):
+    """ Search for a doble quoted words and change it to nothing.
+    this may be used to change LATIN , used in decimal for . used in
+    American notation."""
+    
+    line = list(line)
+    for i in range(len(line)-1):
+        if line[i]== '"':
+            line[i] = ''
+    return ''.join(line)
 
 
 f1 = open('/home/argenis/apps/TL_IO/ventas.csv', 'r')
-f2 = open('ventas.csv.tmp', 'w')
+f2 = open('ventas.csv.tmp1', 'w')
+f3 = open('ventas.csv.tmp', 'w')
 for line in f1:
     f2.write(swapDotComa(line))
+f2.close()
+f2 = open('ventas.csv.tmp1', 'r')
+for line in f2:
+    f3.write(deleteDobleQuoted(line))
 f1.close()
 f2.close()
+f3.close()
 
-shutil.copy("ventas.csv.tmp", "/tmp/ventas.csv.tmp")
-remove("ventas.csv.tmp")
+#shutil.copy("ventas.csv.tmp", "/tmp/ventas.csv.tmp")
+remove("ventas.csv.tmp1")
