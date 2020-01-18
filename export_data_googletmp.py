9db78cd30e1bbe93345808d0759ef002ee23703d
argenis2021 <argenismoncada@gmail.com>
2020-01-18 07:56:19 -0400

    renombrado archivos

--- export_data_googletmp.py
+++ export_data_googletmp.py
@@ -5,14 +5,18 @@ import operator
 import os
 import time
 
-sql = "COPY (select telefono,cliente from all_num_phone) TO STDOUT WITH \
-        CSV DELIMITER ',' header"		# Create the sql senteces in STDOUT
+date = time.strftime("%Y%m%d%H%M%S")		#Define the date
+sql = "COPY (select telefono, cliente\
+       from ventas_la_carlota\
+       where telefono not like ''\
+       group by cliente, telefono) TO STDOUT WITH \
+        CSV DELIMITER ','"		# Create the sql senteces in STDOUT
 conn = psycopg2.connect(host='localhost',		# connect to the database
                         dbname='todolimpiecito',
                         user='postgres',
                         password='')
 cur = conn.cursor()		# cursor object is used to interact with the database
-in_file = open('/home/argenis/apps/TL_IO/all_num_phone.csv', 'w')
+in_file = open('/home/argenis/apps/TL_IO/sms_master_{}.csv'.format(date), 'w')
 # LOAD COPY STARTING AT SECOND LINE IN FILE
 cur.copy_expert(sql, in_file)
 # Cerramos la conexión
